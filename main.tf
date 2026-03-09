c402973eb6b40d5d993aedb0ba4e80fd1b085d82
io.jason.walters <io.jason.walters@usda.gov>
2026-03-09 11:56:43 -0500

    ready to test

--- main.tf
+++ main.tf
@@ -11,11 +11,120 @@ resource "azurerm_resource_group" "hub" {
 # -- Defining the Hub vNet
 resource "azurerm_virtual_network" "this" {
     name                    = "${var.prefix}_vNet"
-    address_space           = values(var.hub_cidrs_v4)
+    address_space           = values(merge(var.hub_cidrs_v4, var.hub_cidrs_v6))
     location                = var.az_reg
     resource_group_name     = azurerm_resource_group.hub.name
     dns_servers             = var.dns_servers
     tags                    = var.tags
 #    bgp_community           = var.ExprRT_BGP_Primary_Community
 }
+
+#---------------------------------------------------------
+# Route Tables
+#-------------------------------------------------------------
+#
+
+resource "azurerm_route_table" "this" {
+    for_each = var.hub_subnets
+        name                            = "${each.key}_UDR"
+        location                        = var.az_reg
+        resource_group_name             = azurerm_resource_group.hub_rsg.name
+        bgp_route_propagation_enabled   = each.key == "GatewaySubnet" || each.key == strcontains(each.key, "Bastion" )  || each.key == strcontains(each.key, "EXT") ? true : false
+        tags                            = var.tags
+}
+
+
+
+
+#---------------------------------------------------------
+# Create Routes for Express_RT_GW pointed to FW floating external IP
+#-------------------------------------------------------------
+#
+
+resource "azurerm_route" "hub_v4" {
+  for_each = var.hub_cidrs_v4  
+    name                    = each.key
+    resource_group_name     = azurerm_resource_group.hub.name
+    route_table_name        = "GatewaySubnet_UDR"
+    address_prefix          = each.value
+    next_hop_type           = "VirtualAppliance" 
+    next_hop_in_ip_address  = var.fw_floating_v4_ips["FW_Float_EXT_v4_IP"]
+}
+
+resource "azurerm_route" "hub_v6" {
+  for_each = var.hub_cidrs_v6
+    name                    = each.key
+    resource_group_name     = azurerm_resource_group.hub.name
+    route_table_name        = "GatewaySubnet_UDR"
+    address_prefix          = each.value
+    next_hop_type           = "VirtualAppliance" 
+    next_hop_in_ip_address  = var.fw_floating_v6_ips["FW_Float_EXT_v6_IP"]
+}
+
+resource "azurerm_route" "spoke_v4" {
+  for_each = var.spoke_cidrs_v4  
+    name                    = each.key
+    resource_group_name     = azurerm_resource_group.hub.name
+    route_table_name        = "GatewaySubnet_UDR"
+    address_prefix          = each.value
+    next_hop_type           = "VirtualAppliance" 
+    next_hop_in_ip_address  = var.fw_floating_v4_ips["FW_Float_EXT_v4_IP"]
+}
+
+resource "azurerm_route" "spoke_v6" {
+  for_each = var.spoke_cidrs_v6
+    name                    = each.key
+    resource_group_name     = azurerm_resource_group.hub.name
+    route_table_name        = "GatewaySubnet_UDR"
+    address_prefix          = each.value
+    next_hop_type           = "VirtualAppliance" 
+    next_hop_in_ip_address  = var.fw_floating_v6_ips["FW_Float_EXT_v6_IP"]
+}
+
+
+
+
+
+#---------------------------------------------------------
+# Create Subnets
+#-------------------------------------------------------------
+#
+
+resource "azurerm_subnet" "this" {
+  for_each = var.hub_subnets
+    name                              = each.key
+    resource_group_name               = azurerm_resource_group.hub_rsg.name
+    virtual_network_name              = azurerm_virtual_network.this.name
+    address_prefixes                  = each.value["Subs"] 
+    service_endpoints                 = contains( each.value["Sub_Svc_Endpoints"], "null" ) ? null : each.value["Sub_Svc_Endpoints"]
+    private_endpoint_network_policies = each.value["priv_endpt"] ? "Disabled" : "Enabled"
+    default_outbound_access_enabled   = each.value["default_outbound_access_enabled"]  
+
+    dynamic "delegation" {
+        for_each = each.value["Sub_Delegation"] ? [1] : [] 
+        content {
+          name = each.value["Sub_Delegation_Name"]
+          service_delegation {
+            actions = each.value["Sub_Delegation_Actions"]
+            name    = each.value["Sub_Delegation_Actions_Name"]
+          }
+        }
+     }
+}
+
+
+
+
+#---------------------------------------------------------
+# Associate Route Tables to subnets
+#-------------------------------------------------------------
+#
+resource "azurerm_subnet_route_table_association" "this" {
+  for_each = var.hub_subnets  
+    subnet_id      = azurerm_subnet.this[each.key].id
+    route_table_id = azurerm_route_table.this[each.key].id
+}
+
+
+
 
--- variables.tf
+++ variables.tf
@@ -23,9 +23,91 @@ variable "hub_cidrs_v4" {
     description = "Map of IPv4 hub CIDRs for region"
 }
 
-/*
 variable "hub_cidrs_v6" {
     type = map(string)
     description = "Map of IPv6 hub CIDRs for region"
 }
-*/
+
+variable "hub_subnets" {
+    description = "subnet definition variable"
+    type = map(object({
+        Subs                            = list(string)
+        Sub_Svc_Endpoints               = list(string)
+        Sub_Delegation                  = bool
+        Sub_Delegation_Name             = string
+        Sub_Delegation_Actions_Name     = string
+        Sub_Delegation_Actions          = list(string)
+        priv_endpt                      = bool
+        default_outbound_access_enabled = bool
+    }))
+}
+
+
+
+
+
+variable "fw1_v4_ips" {
+    type = map(string)
+    description = "FW1 interface IPv4 IPs"
+}
+
+variable "fw1_v6_ips" {
+    type = map(string)
+    description = "FW1 interface IPv6 IPs"
+}
+
+
+variable "fw2_v4_ips" {
+    type = map(string)
+    description = "FW2 interface IPv4 IPs"
+}
+
+variable "fw2_v6_ips" {
+    type = map(string)
+    description = "FW2 interface IPv6 IPs"
+}
+
+
+variable "fw_floating_v4_ips" {
+    type = map(string)
+    description = "FW floating interface IPv4 IPs"
+}
+
+variable "fw_floating_v6_ips" {
+    type = map(string)
+    description = "FW floating interface IPv6 IPs"
+}
+
+
+variable "spoke_cidrs_v4" {
+    type = map(string)
+    description = "Map of IPv4 spoke CIDRs for region"
+}
+
+variable "spoke_cidrs_v6" {
+    type = map(string)
+    description = "Map of IPv6 Spoke CIDRs for region"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
